73837a2968864ec27201f0139dbc44d64362b441
YracemaM <yracemamarquina@gmail.com>
2021-04-13 00:50:00 -0300

    Clase Transporte con pocas lineas de codigo

---
+++ App.java
@@ -0,0 +1,35 @@
+package app.Transporte;
+
+import app.IEsMagico;
+
+public class Transporte extends Wizard implements IEsMagico {
+    
+    private String nombre;
+    
+    //NO CAMBIAR NADA HASTA ACA
+              
+        //Interfase IEsMagico
+        //Implementar en TrenExpressoHowards
+        //Descripcion El Tren es magico
+
+        Transporte TrenEH = new Transporte();
+        trenEH.nombre = "Tren Expresso Howart";
+
+       //Clase: Wizard
+       //Atributo escoba
+       //Tipo Escoba
+       //Descripcion Escoba que usa para transportarse
+
+        Transporte Escoba = new Transporte();
+        Escoba = "Saeta de Fuego";
+
+        
+
+
+
+        
+
+
+
+    }
+}
---
+++ Transporte.java
@@ -0,0 +1,30 @@
+public class Transporte extends wizard implements IEsMagico {
+  
+//consultar con las chicas linea 3
+    public static void main (String [] args) {
+    
+    Transporte transporte1 = new Transporte();
+    Transporte escoba = new Transporte();
+    public String esMagica;
+    public String Modelo;
+
+   
+    Transporte transporte2 = new Transporte();
+    Transporte trenEH = new Transporte();
+}    
+    public String esMagico;
+    public String Volar;
+    
+
+    
+   
+
+}
+
+   
+
+
+
+}
+
+   
